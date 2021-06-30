79001b406121390bacba422014e9f100e5bf7c8b
Husain Haq <Hussainhaq16@gmail.com>
2021-06-30 18:18:16 -0500

    Css and js files
    These 2 files are responsible for the design of the website and how it functions.

---
+++ app.js
@@ -0,0 +1,8 @@
+//Global variables for the the html classes/id
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
